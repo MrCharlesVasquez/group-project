4fd0fe3fc163221e802f88149b457908ebc9e484
Hannah Vaschel <hvaschel@gmail.com>
2019-07-25 17:45:45 -0600

    models are done? routes are coming

---
+++ routes/goalRouter.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const goalRouter = express.Router()
+const Goal = require('../models/goal.js')
+
+
+module.exports = goalRouter
--- server.js
+++ server.js
@@ -4,9 +4,33 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 7000
 
+// * Middleware for every request
 app.use(express.json())
 app.use(morgan('dev'))
 
+
+// * DB connection
+mongoose.connect("mongodb://localhost:27017/vices-rewards", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+    .then(() => console.log("Connected to V/R DB"))
+    .catch(err => console.log(err))
+
+
+// * Routes
+app.use("/transactions", require('./routes/budgetRouter.js'))
+app.use("/goals", require('./routes/goalRouter.js'))
+
+
+// * Global Error Handling
+app.use((err, req, res, next) => {
+    console.error(err)
+    return res.send({ errMsg: err.message })
+})
+
+// * Listen
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
