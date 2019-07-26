9799bc426e0a7d274e6bd52de9b548b4215fcb1e
Hannah Vaschel <hvaschel@gmail.com>
2019-07-26 15:59:01 -0600

    trans-form setup, move methods to context

---
+++ client/src/components/budgetComponents/TransactionForm.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const TransactionForm = props => {
+
+    return(
+        {/* <form onSubmit={""}>
+            <input 
+                type="text" 
+                name="transName" 
+                value={""} 
+                onChange={""} 
+                placeholder="memo"/>
+            <input 
+                type="number" 
+                name="transAmount" 
+                value={""} 
+                onChange={""} 
+                placeholder="amount"/>
+            <input 
+                type="radio" 
+                name="transType" 
+                value="expense" 
+                onChange={""}/>
+            <input 
+                type="radio" 
+                name="transType" 
+                value="income" 
+                onChange={""}/>
+            <input 
+                type="date" 
+                name="transDate" 
+                value={""} 
+                onChange={""} />
+            <button>Submit</button>
+        </form> */}
+    )
+}
+
+
+export default TransactionForm
