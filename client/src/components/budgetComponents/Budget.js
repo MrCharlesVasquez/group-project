5ca9f6d049757369950485fb2db3d5a6b3caff78
n8bertrandgit config --global user.email n8bertrand@gmail.com <n8bertrand@gmail.com>
2019-07-26 13:42:36 -0600

    unneeded branch

--- client/src/components/budgetComponents/Budget.js
+++ client/src/components/budgetComponents/Budget.js
@@ -6,15 +6,20 @@ import TransactionForm from './TransactionForm.js'
 
 
 const Budget = () => {
+    
     const transaction = [
-        {name:'', amount:0, description:'', date:''},
-        {name:'', amount:0, description:'', date:''},
-        {name:'', amount:0, description:'', date:''},
-        {name:'', amount:0, description:'', date:''},
-        {name:'', amount:0, description:'', date:''},
+        {name:'rent', amount:1000, description:'', date:'1'},
+        {name:'car loan', amount:160, description:'', date:'15'},
+        {name:'phone', amount:80, description:'', date:'7'},
+        {name:'gas', amount:50, description:'', date:'17'},
+        {name:'electric', amount:90, description:'', date:'12'},
         {name:'', amount:0, description:'', date:''},
         {name:'', amount:0, description:'', date:''},
     ]
+
+    const mappedBudget = transaction.map((bills) => 
+                <Transaction  {...bills}
+                                 />)
     return (
         <div>
             <TransactionForm/>
