5fdedc2ea29eac19bc93bde824330576d8f90036
Charles Vasquez <charles@mvrealtorsslc.com>
2019-07-31 15:12:29 -0600

    updatedStyleForTransactionForm

--- client/src/components/budgetComponents/Budget.js
+++ client/src/components/budgetComponents/Budget.js
@@ -9,22 +9,25 @@ import { withVice } from '../../context/ViceProvider.js'
 
 const Budget = props => {
     const { total, getTransactions } = props
-    
+
     useEffect(() => {
         // Component Did Mount
         getTransactions()
-    }, [getTransactions] )
+    }, [getTransactions])
 
-    
-    
+
+
     return (
-            <div>
-                <TransactionForm {...props}/>
-                <TransactionsList {...props}/>
-                <h1>Total: {total.toLocaleString(undefined, {minimumFractionDigits:0, maximumFractionDigits:2})}</h1>
-                
+        
+        <div className="BudgetOuterContainer">
+            <div className="BudgetContainer">
+                <TransactionForm {...props} />
+                <TransactionsList {...props} />
+                <h1 className="BudgetTotal" >Total: <span style={{ color: "azure" }}> {total.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 2 })}</span></h1>
+
             </div>
-        )
+        </div>
+    )
 }
 
 export default withVice(Budget)
